3f529c9547db689165ad35838b8ece725ccb6209
katie <knyounke@gmail.com>
2018-05-05 20:45:12 -0500

    finished the threading function and the bubble_threads function for multithreading

--- a2.cpp
+++ a2.cpp
@@ -9,8 +9,17 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
+#include <pthread.h>
+//compile using g++ -std=c++11 -pthread a2.cpp on Linux bash
+//make sure that all_month.csv (the exact file included in the directory) is in the top of the directory
 using namespace std;
+
+struct thread_args{
+	string arr[9822][22];
+	int n;
+	int start;
+
+};
 
 
 void bubbleSort(string arr[][22], int n, int start){
@@ -36,13 +45,50 @@ string temp[22];
    
 	}
   }
-/*	
+/*
  //test to see if working sort
 int p = 0;
 for (p = start; p < n; p++)
 {
  cout<<arr[p][1] << endl;
-} */
+} 
+*/
+}
+
+void *bubble_threads(void* ptr){
+
+struct thread_args * args = (struct thread_args*) ptr;
+
+int i, j;
+string temp[22];
+
+  for(int i = 0; i < (args->n)-1; i++){
+    for(int j = args->start; j < (args->n)-i-1; j++){
+	char * num = const_cast<char*>(args->arr[j][1].c_str());
+	char * num2 = const_cast<char*>(args->arr[j+1][1].c_str());
+       	  if(strtof(num,0) > strtof(num2,0)){
+	     for(int k = 0; k < 22; k++){
+	     temp[k] = args->arr[j][k];	
+	     }
+	     for(int k = 0; k < 22; k++){
+		args->arr[j][k] = args->arr[j+1][k];
+	     }
+             for(int k = 0; k < 22; k++){
+		args->arr[j+1][k] = temp[k];	
+	     }
+	
+	  }	
+   
+	}
+  }
+/*
+ //test to see if working sort
+int p = 0;
+for (p = start; p < n; p++)
+{
+ cout<<arr[p][1] << endl;
+} 
+*/
 }
 
 void twoProcess(string arr[][22]){
@@ -58,18 +104,93 @@ void twoProcess(string arr[][22]){
 		}
 	}
 	
-	if(pid != 0){//this part is not getting sorted for some reason
+	if(pid != 0){
 	bubbleSort(arr,4911,4911);
 	wait(NULL);
 	 	
 	}	
 }
 
+
+void fourProcess(string arr[][22]){
+
+	pid_t pid;
+	pid_t pid2;
+
+	pid = fork();
+	if(pid ==0)
+	 bubbleSort(arr, 2455, 0);
+	 
+	pid2 = fork();
+	if (pid2 == 0)
+	 bubbleSort(arr, 2456, 2456);
+	
+	
+	if(pid != 0){
+	 bubbleSort(arr, 2455,  4911);	
+	bubbleSort(arr, 2456, 7366);
+	}	
+		
+}
+
+void ten_fork(string arr[][22], int n){
+
+	pid_t pid;
+	
+	if(n > 0)
+	{
+	   if((pid = fork()) < 0)
+		{
+			cout << "error";
+		}
+	   else if (pid == 0)
+		{
+		bubbleSort(arr,982, 0);
+		bubbleSort(arr, 983, 983);
+		bubbleSort(arr, 982, 1965);
+		bubbleSort(arr, 982, 2947);
+	        bubbleSort(arr,982,3929);
+		bubbleSort(arr,983,4911);
+		bubbleSort(arr,982,5894);	
+		bubbleSort(arr,982,6876);
+		bubbleSort(arr,982,7858);
+		bubbleSort(arr,982,8840);
+		}
+	   else if(pid > 0)
+		{
+		  ten_fork(arr,n-1);
+		}
+
+	}
+	
+	
+}
+
+
+void threading(string arr[][22], int num_threads){
+int i;
+pthread_t threads[num_threads];
+int rc;
+   for(i = 0; i < num_threads; i++){
+	struct thread_args args;
+	args.n = 982;
+	args.start = (9822/num_threads);
+	for(int j = 0; j < 9822; j++){
+	   for(int k = 0; k < 22; k++){	
+			args.arr[j][k] = arr[j][k]; 
+		}
+	}	
+	rc =pthread_create(&threads[i],NULL,bubble_threads, (void*)&args);	
+    }
+
+}
+
+
 int main() 
 {
 
 	
-   	 time_t startTime;
+   	time_t startTime;
 	time_t final;
 	time_t endTime;
 	
@@ -107,19 +228,64 @@ int main()
 	}
 	i++;
 	}
-/*
+	char num;
+	cout << "The last time listed is the total time. " << endl
+	<< "Enter the amount you would like to sort on: " << endl;
+	cout << "1. One process / 1 thread" << endl;
+	cout << "2. Two processes" << endl;
+	cout << "3. Four processes" << endl;
+	cout << "4. Ten processes" << endl;
+	cout << "5. 2 threads" << endl;
+	cout << "6. 4 threads" << endl;
+	cout << "7. 10 threads" << endl;
+	cout << "8. Exit" << endl;
+	cin >> num;
+
+switch(num){
+
+  case '1': 
+
 	//time for one process
 	time(&startTime);		
 	bubbleSort(earthquakes,9822,0);
 	time(&endTime);
 	final = difftime(endTime,startTime); 
 	cout << final << " seconds"; 
-*/
+	break;
+  case '2':
 	
 	time(&startTime);
 	twoProcess(earthquakes);
 	time(&endTime);
 	final = difftime(endTime,startTime);
 	cout << final << " seconds";
+	break;
+  case '3':
+	time(&startTime);
+	fourProcess(earthquakes);
+	time(&endTime);
+	final = difftime(endTime,startTime);
+	cout << final << " seconds";
+	break;
+  case '4':
+
+	time(&startTime);
+	ten_fork(earthquakes, 10);
+	time(&endTime);
+	final = difftime(endTime,startTime);
+	cout << final << " seconds";
+  case '5':
+	threading(earthquakes, 2);
+	break;
+  case '6':
+	threading(earthquakes,4);
+	break;
+  default:
+	exit(0);
+
+}
+
+pthread_exit(NULL);	
+
 return 0;
 }
